c7ae0c2e5f8b23a743cb52008fa55c080908368d
Riley <riley@rileybathurst.com>
2024-07-20 12:51:04 -0700

    year loop

--- src/lib/strapi.ts
+++ src/lib/strapi.ts
@@ -13,16 +13,6 @@ interface fetchApiTypes {
   };
 }
 
-/**
- * Fetches data from the Strapi API
- * @param endpoint - The endpoint to fetch from
- * @param query - The query parameters to add to the url
- * @param wrappedByKey - The key to unwrap the response from
- * @param wrappedByList - If the response is a list, unwrap it
- * @param populate - The fields to populate
- * @returns
- */
-
 export default async function fetchApi<T>({
   endpoint,
   query,
